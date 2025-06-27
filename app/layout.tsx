94ca57d4de13c550018056b12e5cffa215d77403
estebantapia-encora <esteban.tapia@encora.com>
2025-06-27 08:37:51 -0700

    style: apply visual adjustments for light/dark mode as outlined in #2

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR" className="dark">
+    <html lang="pt-BR">
       <body className={inter.className}>
         <AuthProvider>
           <div className="app-container">
